a0b42646f91d26bc8f722952146e13cce2a00553
Chiang <Chiangzg@gmail.com>
2017-05-11 19:05:13 +0800

    增加英文字体库，渲染字符串

--- src/bootpack.c
+++ src/bootpack.c
@@ -32,35 +32,29 @@ void set_palette(int start, int end, unsigned char *rgb);
 
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
+void init_screen(char *vram, int x, int y);
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+
+struct BOOTINFO {
+    char cyls, leds, vmode, reserve;
+    short scrnx, scrny;
+    char *vram;
+};
+
 /**
  * main
  */
 void HariMain(void) {
-    char *vram;
-    int xsize, ysize;
+    struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+    extern char hankaku[4096];
 
     init_palette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 
-    vram = (char *) 0xa0000;
-    xsize = 320;
-    ysize = 200;
-
-    boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
-    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
-    boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
-    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
-
-    boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
-    boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
-    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
-    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
-    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
-    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
-
-    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
-    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
-    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
-    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 125, 1, COL8_848400, "Amiao OS.");
 
     for (;;) {
         io_hlt();
@@ -87,6 +81,55 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
     }
     return;
 }
+
+
+/**
+ * 单字体渲染
+ *
+ * @param vram
+ * @param xsize
+ * @param x
+ * @param y
+ * @param c
+ * @param font
+ */
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font) {
+    int i;
+    char *p, d /* data */;
+    for (i = 0; i < 16; i++) {
+        p = vram + (y + i) * xsize + x;
+        d = font[i];
+        if ((d & 0x80) != 0) { p[0] = c; }
+        if ((d & 0x40) != 0) { p[1] = c; }
+        if ((d & 0x20) != 0) { p[2] = c; }
+        if ((d & 0x10) != 0) { p[3] = c; }
+        if ((d & 0x08) != 0) { p[4] = c; }
+        if ((d & 0x04) != 0) { p[5] = c; }
+        if ((d & 0x02) != 0) { p[6] = c; }
+        if ((d & 0x01) != 0) { p[7] = c; }
+    }
+    return;
+}
+
+/**
+ * 字符串渲染
+ *
+ * @param vram      显卡vram
+ * @param xsize     x轴方向
+ * @param x         x坐标
+ * @param y         y坐标
+ * @param c         颜色
+ * @param s         渲染的字符串
+ */
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s) {
+    extern char hankaku[4096];
+    for (; *s != 0x00; s++) {
+        putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+        x += 8;
+    }
+    return;
+}
+
 
 /**
  *  初始化调色板
@@ -138,5 +181,32 @@ void set_palette(int start, int end, unsigned char *rgb) {
         rgb += 3;
     }
     io_store_eflags(eflags);    //恢复中断标志
+    return;
+}
+
+/**
+ * 渲染屏幕背景
+ *
+ * @param vram
+ * @param x
+ * @param y
+ */
+void init_screen(char *vram, int x, int y) {
+    boxfill8(vram, x, COL8_008484, 0, 0, x - 1, y - 29);
+    boxfill8(vram, x, COL8_C6C6C6, 0, y - 28, x - 1, y - 28);
+    boxfill8(vram, x, COL8_FFFFFF, 0, y - 27, x - 1, y - 27);
+    boxfill8(vram, x, COL8_C6C6C6, 0, y - 26, x - 1, y - 1);
+
+    boxfill8(vram, x, COL8_FFFFFF, 3, y - 24, 59, y - 24);
+    boxfill8(vram, x, COL8_FFFFFF, 2, y - 24, 2, y - 4);
+    boxfill8(vram, x, COL8_848484, 3, y - 4, 59, y - 4);
+    boxfill8(vram, x, COL8_848484, 59, y - 23, 59, y - 5);
+    boxfill8(vram, x, COL8_000000, 2, y - 3, 59, y - 3);
+    boxfill8(vram, x, COL8_000000, 60, y - 24, 60, y - 3);
+
+    boxfill8(vram, x, COL8_848484, x - 47, y - 24, x - 4, y - 24);
+    boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y - 4);
+    boxfill8(vram, x, COL8_FFFFFF, x - 47, y - 3, x - 4, y - 3);
+    boxfill8(vram, x, COL8_FFFFFF, x - 3, y - 24, x - 3, y - 3);
     return;
 }
