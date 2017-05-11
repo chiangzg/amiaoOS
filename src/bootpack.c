9d3821a9150ad20179a65db028ac613f6fdc5b4c
Chiang <Chiangzg@gmail.com>
2017-05-11 16:43:20 +0800

    增加矩形算法

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,3 +1,21 @@
+//16色定义
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
 void io_hlt(void);
 
 void io_cli(void);
@@ -12,25 +30,70 @@ void init_palette(void);
 
 void set_palette(int start, int end, unsigned char *rgb);
 
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+/**
+ * main
+ */
 void HariMain(void) {
-    int i;
-    char *p;
+    char *vram;
+    int xsize, ysize;
 
     init_palette();
 
-    p = (char *) 0xa0000;
+    vram = (char *) 0xa0000;
+    xsize = 320;
+    ysize = 200;
 
-    for (i = 0; i <= 0xffff; i++) {
-        p[i] = i & 0x0f;
-    }
+    boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
+    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
+    boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
+    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
+
+    boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
+    boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
+    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
+    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
 
     for (;;) {
         io_hlt();
     }
 }
 
+/**
+ * 填充矩形
+ *
+ * @param vram
+ * @param xsize
+ * @param c
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ */
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1) {
+    int x, y;
+    for (y = y0; y <= y1; y++) {
+        for (x = x0; x <= x1; x++)
+            vram[y * xsize +
+                 x] = c;    //像素点算法。对于320*200 左上点（0，0） 右下点（319，199）哪儿（x，y）坐标对应的VRAM内存地址为    0xa0000(VRAM起始地址) + x + y * 320
+    }
+    return;
+}
+
+/**
+ *  初始化调色板
+ */
 void init_palette(void) {
     static unsigned char table_rgb[16 * 3] = {
+            //rgb
             0x00, 0x00, 0x00,
             0xff, 0x00, 0x00,
             0x00, 0xff, 0x00,
@@ -54,17 +117,26 @@ void init_palette(void) {
 
 }
 
+/**
+ * 初始化显卡VRAM
+ *
+ * @param start 起始颜色
+ * @param end   截止颜色
+ * @param rgb
+ */
 void set_palette(int start, int end, unsigned char *rgb) {
     int i, eflags;
-    eflags = io_load_eflags();
-    io_cli();
+    eflags = io_load_eflags();  //记录中断标志
+    io_cli();   //禁止中断
     io_out8(0x03c8, start);
+
+    //写入16位颜色
     for (i = start; i <= end; i++) {
         io_out8(0x03c9, rgb[0] / 4);
         io_out8(0x03c9, rgb[1] / 4);
         io_out8(0x03c9, rgb[2] / 4);
         rgb += 3;
     }
-    io_store_eflags(eflags);
+    io_store_eflags(eflags);    //恢复中断标志
     return;
 }
